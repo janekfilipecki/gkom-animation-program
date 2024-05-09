e78dfb8fe983d7b5d6dda68e71b95250dc7f8152
janekfilipecki <filipeckijan@gmail.com>
2024-05-09 17:01:19 +0200

    added main test window

---
+++ main.py
@@ -0,0 +1,33 @@
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+
+def main():
+    pygame.init()
+    display = (800, 600)
+    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+
+    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
+    glTranslatef(0.0, 0.0, -5)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        glRotatef(1, 3, 1, 1)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glBegin(GL_TRIANGLES)
+        glVertex3fv((0, 1, 0))
+        glVertex3fv((-1, -1, 0))
+        glVertex3fv((1, -1, 0))
+        glEnd()
+        pygame.display.flip()
+        pygame.time.wait(10)
+
+
+if __name__ == "__main__":
+    main()
