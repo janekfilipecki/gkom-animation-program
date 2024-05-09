97700dde5ee458880cb089e38e4dc0762bc92050
bartek <bniewiar>
2024-05-09 22:58:19 +0200

    change way to load file

--- main.py
+++ main.py
@@ -6,30 +6,39 @@ from OpenGL.GL import (glClear, GL_COLOR_BUFFER_BIT, GL_DEPTH_BUFFER_BIT,
 from OpenGL.GLU import gluPerspective
 from loadFile import draw_model, load_obj
 import sys
+import tkinter as tk
+from tkinter import filedialog
 
 
 def main():
-    if len(sys.argv) >= 0:
-        obj_path = sys.argv[1]
-        pygame.init()
-        display = (800, 600)
-        pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+    pygame.init()
+    display = (800, 600)
+    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
 
-        gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
-        glTranslatef(0.0, 0.0, -5)
-        vertices, faces = load_obj(obj_path)  # Specify your .obj file here
+    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
+    glTranslatef(0.0, 0.0, -5)
 
-        while True:
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    pygame.quit()
-                    quit()
+    vertices = []
+    faces = []
 
-            glRotatef(1, 3, 1, 1)
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-            draw_model(vertices, faces)
-            pygame.display.flip()
-            pygame.time.wait(10)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_l:  # Press 'L' key to load a new model
+                    root = tk.Tk()
+                    root.withdraw()
+                    file_path = filedialog.askopenfilename(filetypes=[("OBJ Files", "*.obj")])
+                    if file_path:
+                        vertices, faces = load_obj(file_path)
+
+        glRotatef(1, 3, 1, 1)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        draw_model(vertices, faces)
+        pygame.display.flip()
+        pygame.time.wait(10)
 
 
 if __name__ == "__main__":
